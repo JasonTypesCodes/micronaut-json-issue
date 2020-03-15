e3b40fdca1252649785d0ead6a74af8951ddd3f6
Jason Schindler <schindlerj@objectcomputing.com>
2020-03-15 16:49:35 -0500

    Removing old projects

--- some-service/src/main/java/some/service/SomethingElseRepository.java
+++
@@ -1,7 +0,0 @@
-package some.service;
-
-import io.micronaut.data.annotation.Repository;
-import io.micronaut.data.jpa.repository.JpaRepository;
-
-@Repository
-public interface SomethingElseRepository extends JpaRepository<SomethingElse, Long> {}
--- some-service/src/main/java/some/service/SomethingRepository.java
+++
@@ -1,7 +0,0 @@
-package some.service;
-
-import io.micronaut.data.annotation.Repository;
-import io.micronaut.data.jpa.repository.JpaRepository;
-
-@Repository
-public interface SomethingRepository extends JpaRepository<Something, Long> {}
