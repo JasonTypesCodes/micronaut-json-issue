a838c7b75396272a53af164c5fb8d497ecdee64d
Jason Schindler <schindlerj@objectcomputing.com>
2020-03-15 21:51:22 -0500

    Adds entities to JHipster application

--- jhipster-micronaut/src/main/webapp/app/entities/entity.module.ts
+++ jhipster-micronaut/src/main/webapp/app/entities/entity.module.ts
@@ -4,6 +4,18 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
+      {
+        path: 'bank-account',
+        loadChildren: () => import('./bank-account/bank-account.module').then(m => m.JhipsterMicronautBankAccountModule)
+      },
+      {
+        path: 'label',
+        loadChildren: () => import('./label/label.module').then(m => m.JhipsterMicronautLabelModule)
+      },
+      {
+        path: 'operation',
+        loadChildren: () => import('./operation/operation.module').then(m => m.JhipsterMicronautOperationModule)
+      }
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ])
   ]
